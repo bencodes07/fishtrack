61a4e32084ac4cdf4e54aad02668cfd437232dfc
Ben Böckmann <boeckmannben@gmail.com>
2023-07-03 00:29:04 +0200

    Navbar Finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Home from "./components/Home";
-import Collection from "./components/Collection.jsx"
+import Collection from "./components/Collection.jsx";
 import { AuthContextProvider } from "./contexts/AuthContextProvider";
 
 function App() {
   return (
-    <div className="App">
-      <AuthContextProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/collection/:name" element={<Collection />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/signup" element={<Signup />}></Route>
-          </Routes>
-        </BrowserRouter>
-      </AuthContextProvider>
-    </div>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/collection/:name" element={<Collection />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthContextProvider>
   );
 }
 
--- src/components/loggedOut/HomeOut.jsx
+++ src/components/loggedOut/HomeOut.jsx
@@ -1,22 +1,52 @@
+import { FiLogIn } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+
 function HomeOut() {
+  const navigate = useNavigate();
   return (
     <>
-      <nav className="flex justify-between w-screen h-[140px]">
-        <div>
-          <h1 className="font-logo">fishtrack.</h1>
+      <nav className="flex justify-between items-center w-screen h-[100px]">
+        <div className="flex justify-end w-[10vw] min-w-fit ml-[5vw]">
+          <h1 className="font-logo text-4xl text-[#003585]">fishtrack.</h1>
         </div>
         <div>
-          <ul className="flex">
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">FRIENDS</a></li>
-            <li><a href="#">INVITE</a></li>
+          <ul className="flex text-lg w-[30vw] max-w-[440px] justify-between items-center list-none max-lg:w-[35vw]">
+            <li className="">
+              <a
+                className="text-black hover:no-underline hover:text-black hover:font-medium transition-all"
+                href="#"
+              >
+                HOME
+              </a>
+            </li>
+            <li className=" text-[#7F7F7F]">
+              <a
+                className="hover:no-underline hover:text-black hover:font-medium transition-all"
+                href="#"
+              >
+                FRIENDS
+              </a>
+            </li>
+            <li className="text-[#7F7F7F]">
+              <a
+                className="hover:no-underline hover:text-black hover:font-medium transition-all"
+                href="#"
+              >
+                INVITE
+              </a>
+            </li>
           </ul>
         </div>
-        <div>
-          <button>LOGIN</button>
+        <div className="w-[10vw] mr-[5vw] flex justify-start min-w-fit">
+          <button
+            onClick={() => navigate("/login")}
+            className="flex justify-center items-center rounded-full border-[#003585] border-2 text-[#003585] p-2"
+          >
+            <FiLogIn size={18} className="mx-1" />
+            <p className="mx-1 text-lg">LOGIN</p>
+          </button>
         </div>
       </nav>
-
     </>
   );
 }
