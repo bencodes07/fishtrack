f46b66907441dc4b7d85712a8b6529c83595ee84
bencodes07 <boeckmannben@gmail.com>
2023-07-14 17:40:36 +0100

    Minor bug fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,9 @@ function Footer() {
   return (
     <div className="bg-[#003585] w-screen h-[50px] absolute bottom-0 flex justify-evenly items-center">
       <h1 className="font-logo text-2xl text-[#fff]">fishtrack. </h1>{" "}
-      <p className="text-white max-sm:hidden">© 2023 Copyright: Ben Böckmann</p>
+      <p className="text-white max-sm:hidden ">
+        © 2023 Copyright: Ben Böckmann
+      </p>
     </div>
   );
 }
