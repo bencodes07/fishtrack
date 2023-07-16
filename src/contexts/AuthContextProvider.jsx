8755328c36250a402bb797199312944c58171a95
bencodes07 <boeckmannben@gmail.com>
2023-07-16 21:56:47 +0100

    Vercel update

--- src/contexts/AuthContextProvider.jsx
+++ src/contexts/AuthContextProvider.jsx
@@ -1,11 +1,4 @@
 import { useContext, createContext, useEffect, useState } from "react";
-import {
-  signOut,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  sendEmailVerification,
-} from "firebase/auth";
 import { auth } from "../firebase/config";
 
 const AuthContext = createContext();
@@ -15,13 +8,14 @@ export const AuthContextProvider = ({ children }) => {
   const [error, setError] = useState("");
 
   const signIn = (email, password) => {
-    signInWithEmailAndPassword(auth, email, password)
+    auth
+      .signInWithEmailAndPassword(email, password)
       .then((user) => {
         if (!user.user.emailVerified) {
           console.error("Email not verified");
           setError("Email not verified");
           document.getElementById("error").style.color = "#ff0000";
-          signOut(auth);
+          auth.signOut();
         }
         document.getElementById("error").style.color = "#008000";
         setError("Success!");
@@ -33,24 +27,22 @@ export const AuthContextProvider = ({ children }) => {
   };
 
   const logOut = () => {
-    signOut(auth);
+    auth.signOut();
   };
 
   const signUp = async (email, password) => {
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    ).catch((err) => {
-      setError(err.message);
-    });
-    await sendEmailVerification(userCredential.user);
-    signOut(auth);
+    const userCredential = await auth
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        setError(err.message);
+      });
+    /* await auth.sendEmailVerification(userCredential.user); */
+    auth.signOut();
     return userCredential;
   };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
       setUser(currentUser);
       console.log("User", currentUser);
     });
