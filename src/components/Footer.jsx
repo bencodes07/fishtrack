9a27a616936c09ec28819171e954a8c0a7692287
bencodes07 <boeckmannben@gmail.com>
2023-07-17 11:51:26 +0100

    Footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const pageHeight = window.innerHeight;
   return (
     <div
-      className="bg-[#003585] w-screen h-[50px] absolute flex justify-evenly items-center"
+      className="bg-[#003585] w-screen h-[50px] flex justify-evenly items-center"
       style={{ top: (pageHeight - 50).toString() + "px" }}
     >
       <h1 className="font-logo text-2xl text-[#fff]">fishtrack. </h1>{" "}
