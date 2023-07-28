a4d7f8d95d0abbf92459d2a44aa53a6f9c6a8655
bencodes07 <boeckmannben@gmail.com>
2023-07-28 21:54:21 +0200

    Changed title

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,10 @@ function Footer() {
       style={{ top: (pageHeight - 50).toString() + "px" }}
     >
       <h1 className="font-logo text-2xl text-[#fff]">fishtrack. </h1>{" "}
+      <select name="langSelect" id="langSelect">
+        <option value="german">Deutsch</option>
+        <option value="english">English</option>
+      </select>
       <p className="text-white max-sm:hidden ">
         © 2023 Copyright: Ben Böckmann
       </p>
