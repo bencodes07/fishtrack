45367120269b047835de52915d43bd6f357d76a9
bencodes07 <boeckmannben@gmail.com>
2023-07-28 14:33:05 +0200

    Sitemap config added

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import Pages from "vite-plugin-pages";
 import generateSitemap from "vite-plugin-pages-sitemap";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
