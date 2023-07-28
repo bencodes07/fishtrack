77ade920e1a39c79d384fe7e6c165c0b005d7141
bencodes07 <boeckmannben@gmail.com>
2023-07-28 14:39:42 +0200

    Sitemap written

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import Pages from "vite-plugin-pages";
-import generateSitemap from "vite-plugin-pages-sitemap";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    Pages({
-      onRoutesGenerated: async (routes) => {
-        generateSitemap({
-          hostname: "https://fishtrack.net/",
-          routes: [...routes],
-          readable: true,
-        });
-      },
-    }),
-  ],
+  plugins: [react()],
   base: "/",
 });
